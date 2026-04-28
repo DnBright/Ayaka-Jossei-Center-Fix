82983cdad6337713ee35c597c0c637d508695ad5
DnBright <dnbright000@gmail.com>
2026-04-29 02:27:04 +0700

    👤 Add Writer User Seeder

---
+++ database/seeders/WriterUserSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class WriterUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::updateOrCreate(
+            ['email' => 'penulis@ayakajosei.com'], // Email unik untuk login
+            [
+                'name' => 'penulis_ayaka',
+                'password' => Hash::make('Ayaka@Penulis2026'),
+                'role' => 'penulis', // Sesuaikan dengan sistem role Anda
+            ]
+        );
+    }
+}
