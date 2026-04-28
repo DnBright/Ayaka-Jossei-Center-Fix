fd44d43e9f672a06795e57539011d0d980ebb2b2
DnBright <dnbright000@gmail.com>
2026-04-28 22:49:36 +0700

    “update”

--- database/migrations/2026_04_28_150620_create_pages_table.php
+++ database/migrations/2026_04_28_150620_create_pages_table.php
@@ -6,20 +6,17 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('pages', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->string('slug')->unique();
+            $table->json('content')->nullable();
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('pages');
