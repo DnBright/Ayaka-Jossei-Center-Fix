92a77d582a74acc4ffa5b9dbd35e1fdc15d32d3e
DnBright <dnbright000@gmail.com>
2026-04-28 18:47:28 +0700

    update

--- database/migrations/2026_04_28_114446_create_alumni_table.php
+++ database/migrations/2026_04_28_114446_create_alumni_table.php
@@ -6,20 +6,20 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('alumni', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('batch')->nullable();
+            $table->string('working_at')->nullable(); // Company or Location
+            $table->text('testimonial')->nullable();
+            $table->string('image_url')->nullable();
+            $table->boolean('is_featured')->default(false);
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('alumni');
